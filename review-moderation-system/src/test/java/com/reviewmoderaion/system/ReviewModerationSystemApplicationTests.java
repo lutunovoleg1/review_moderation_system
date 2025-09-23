bf0fac4ce0fd7003f0fb69c808b4170febefcc56
Vladislav Pridchin <lumoskiofficial@gmail.com>
2025-09-23 19:13:32 +0300

    feat: initial Spring Boot setup for review moderation system

---
+++ review-moderation-system/src/test/java/com/reviewmoderaion/system/ReviewModerationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviewmoderaion.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewModerationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
